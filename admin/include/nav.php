b3a50c70664c953d6d260535aaa3a690dc4647a0
maywadymin12345 <capital.maywadymin@gmail.com>
2022-03-04 15:27:42 +0630

    adminto view link

--- admin/include/nav.php
+++ admin/include/nav.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="dashboard.php"><strong><i class="icon fa fa-calendar"></i> Report</strong></a>
+        <a class="navbar-brand" href="../../"><strong><i class="icon fa fa-calendar"></i> Report</strong></a>
         <div id="sideNav" href="">
             <i class="fa fa-bars icon"></i> 
         </div>
@@ -50,41 +50,6 @@
                 <a href="#"><i class="fa fa-user"></i>User Profile</a>
             </li>
             
-            
-           
-            <!--<li>
-                <a href="form.php"><i class="fa fa-edit"></i> Forms </a>
-            </li> -->
-            <!-- <li>
-                <a href="#"><i class="fa fa-sitemap"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="#">Second Level Link</a>
-                    </li>
-                    <li>
-                        <a href="#">Second Level Link</a>
-                    </li>
-                    <li>
-                        <a href="#">Second Level Link<span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Link</a>
-                            </li>
-
-                        </ul>
-
-                    </li>
-                </ul>
-            </li> -->
-            <!-- <li>
-                <a href="empty.html"><i class="fa fa-fw fa-file"></i> Empty Page</a>
-            </li> -->
         </ul>
 
     </div>
