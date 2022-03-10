2ae1c3fb30b7fe2507c398b99d38bbf4952a69ae
maywadymin12345 <capital.maywadymin@gmail.com>
2022-03-11 01:14:40 +0630

    hamburger menu

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -5,18 +5,18 @@
 <script src="../assets/js/bootstrap.min.js"></script>
 <!-- Metis Menu Js -->
 <script src="../assets/js/jquery.metisMenu.js"></script>
-    <!-- DATA TABLE SCRIPTS -->
+<!-- DATA TABLE SCRIPTS -->
 <script src="../assets/js/dataTables/jquery.dataTables.js"></script>
 <script src="../assets/js/dataTables/dataTables.bootstrap.js"></script> 
-<!-- <script src="../assets/summernote.min.js"></script> -->
-<!-- <script type="text/javascript" src="../assets/tinymce/tinymce.min.js"></script> -->
+<!-- Custom Js -->
+<script src="../assets/js/custom-scripts.js"></script>
+
 <script>
     $(document).ready(function () {
         $('#dataTables-example').dataTable();
     });
 </script>
-        <!-- Custom Js -->
-<script src="assets/js/custom-scripts.js"></script>
+
 
 <script type="text/javascript">
     // tinymce.init({
